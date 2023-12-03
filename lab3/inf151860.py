8ab5966bd218b6a7215e735b7c70741afcd48047
bartox7777 <bartosz.zelek@protonmail.com>
2023-12-03 23:48:37 +0100

    Add lab3/sprawozdanie2.tex and update lab2/my-gradients.pdf

--- lab3/inf151860.py
+++ lab3/inf151860.py
@@ -1,71 +1,88 @@
 import warnings
+
+from matplotlib.pylab import det
 warnings.simplefilter("ignore")
 
 import scipy.io.wavfile as wav
 import scipy
 import numpy as np
-import sys
+import sys, os
 from scipy.signal import medfilt
 
 # rozpoznanie płci osoby mówiącej metodą cepstralną
 
 good = 0
 bad = 0
+err_matrix = [[0, 0], [0, 0]]
 
-try:
-    file = sys.argv[1]
-except:
-    print("Podaj nazwę pliku w pierwszym argumencie")
-    exit(-1)
+# try:
+#     file = sys.argv[1]
+# except:
+#     print("Podaj nazwę pliku w pierwszym argumencie")
+#     exit(-1)
 
-fs, data = wav.read(file) # wczytanie pliku
+for file in os.listdir("train"):
+    fs, data = wav.read(f"train/{file}") # wczytanie pliku
 
-if len(data.shape) > 1:
-    data = data[:, 0] # jeśli plik jest stereo, to bierzemy tylko jeden kanał
+    if len(data.shape) > 1:
+        data = data[:, 0] # jeśli plik jest stereo, to bierzemy tylko jeden kanał
 
-try:    
-    data = medfilt(data, 3) # filtr medianowy
-except:
-    pass
-    
-freqs = np.fft.fftfreq(len(data), 1/fs)  # obliczenie częstotliwości
-    
-window = scipy.signal.windows.blackman(len(data)) # okno blackmana
+    try:
+        data = medfilt(data, 3) # filtr medianowy
+    except:
+        pass
 
-data = data * window # zastosowanie okna
+    freqs = np.fft.fftfreq(len(data), 1/fs)  # obliczenie częstotliwości
 
-spectrum = np.fft.fft(data) # obliczenie spektrum
+    window = scipy.signal.windows.flattop(len(data)) # okno flattopa
 
-log_spectrum = np.log(np.abs(spectrum)) # logarytmowanie
+    data = data * window # zastosowanie okna
 
-cepstrum = np.abs(np.fft.fft(log_spectrum)) # obliczenie cepstrum
+    spectrum = np.fft.fft(data) # obliczenie spektrum
 
-cepstrum_freqs = np.fft.fftfreq(len(log_spectrum), (freqs[1]-freqs[0])) # obliczenie częstotliwości cepstrum
+    log_spectrum = np.log(np.abs(spectrum)) # logarytmowanie
 
-human_freqs_range = (50, 300)
+    cepstrum = np.abs(np.fft.fft(log_spectrum)) # obliczenie cepstrum
 
-limited_cepstrum_freqs = (cepstrum_freqs > 1/human_freqs_range[1]) & (cepstrum_freqs <= 1/human_freqs_range[0]) # ograniczenie zakresu częstotliwości
+    cepstrum_freqs = np.fft.fftfreq(len(log_spectrum), (freqs[1]-freqs[0])) # obliczenie częstotliwości cepstrum
 
-peak_idx = np.argmax(cepstrum[limited_cepstrum_freqs]) # znalezienie indeksu największej wartości w ograniczonym zakresie
+    human_freqs_range = (50, 300)
 
-peak_freq = 1/cepstrum_freqs[limited_cepstrum_freqs][peak_idx] # znalezienie częstotliwości odpowiadającej indeksowi
+    limited_cepstrum_freqs = (cepstrum_freqs > 1/human_freqs_range[1]) & (cepstrum_freqs <= 1/human_freqs_range[0]) # ograniczenie zakresu częstotliwości
 
-real_gender = file.split(".")[0][-1]
+    peak_idx = np.argmax(cepstrum[limited_cepstrum_freqs]) # znalezienie indeksu największej wartości w ograniczonym zakresie
 
-if peak_freq <= 170:
-    detected_gender = "M"
-else:
-    detected_gender = "K"
+    peak_freq = 1/cepstrum_freqs[limited_cepstrum_freqs][peak_idx] # znalezienie częstotliwości odpowiadającej indeksowi
 
-if real_gender == detected_gender:
-    good += 1
-else:
-    bad += 1
-    
+    real_gender = file.split(".")[0][-1]
+
+    if peak_freq <= 170:
+        detected_gender = "M"
+    else:
+        detected_gender = "K"
+
+    if real_gender == "M":
+        if detected_gender == "M":
+            err_matrix[0][0] += 1
+        else:
+            err_matrix[0][1] += 1
+    else:
+        if detected_gender == "K":
+            err_matrix[1][1] += 1
+        else:
+            err_matrix[1][0] += 1
+            
+
+    if real_gender == detected_gender:
+        good += 1
+    else:
+        bad += 1
+
 
 print(good)
 print(bad)
-print(detected_gender)
+# print(detected_gender)
+print(err_matrix)
 
 ##########
 
