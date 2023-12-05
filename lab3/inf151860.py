1bac5e354a7670f39c572b8aeb6a02b6c234f1f8
bartox7777 <bartosz.zelek@protonmail.com>
2023-12-05 23:44:38 +0100

    Add gender classification using cepstral analysis

--- lab3/inf151860.py
+++ lab3/inf151860.py
@@ -1,6 +1,5 @@
 import warnings
 
-from matplotlib.pylab import det
 warnings.simplefilter("ignore")
 
 import scipy.io.wavfile as wav
@@ -11,231 +10,67 @@ from scipy.signal import medfilt
 
 # rozpoznanie płci osoby mówiącej metodą cepstralną
 
-good = 0
-bad = 0
-err_matrix = [[0, 0], [0, 0]]
+# err_matrix = [[0, 0], [0, 0]]
 
-# try:
-#     file = sys.argv[1]
-# except:
-#     print("Podaj nazwę pliku w pierwszym argumencie")
-#     exit(-1)
-
-for file in os.listdir("train"):
-    fs, data = wav.read(f"train/{file}") # wczytanie pliku
-
-    if len(data.shape) > 1:
-        data = data[:, 0] # jeśli plik jest stereo, to bierzemy tylko jeden kanał
-
-    try:
-        data = medfilt(data, 3) # filtr medianowy
-    except:
-        pass
-
-    freqs = np.fft.fftfreq(len(data), 1/fs)  # obliczenie częstotliwości
-
-    window = scipy.signal.windows.flattop(len(data)) # okno flattopa
-
-    data = data * window # zastosowanie okna
-
-    spectrum = np.fft.fft(data) # obliczenie spektrum
-
-    log_spectrum = np.log(np.abs(spectrum)) # logarytmowanie
-
-    cepstrum = np.abs(np.fft.fft(log_spectrum)) # obliczenie cepstrum
-
-    cepstrum_freqs = np.fft.fftfreq(len(log_spectrum), (freqs[1]-freqs[0])) # obliczenie częstotliwości cepstrum
-
-    human_freqs_range = (50, 300)
-
-    limited_cepstrum_freqs = (cepstrum_freqs > 1/human_freqs_range[1]) & (cepstrum_freqs <= 1/human_freqs_range[0]) # ograniczenie zakresu częstotliwości
-
-    peak_idx = np.argmax(cepstrum[limited_cepstrum_freqs]) # znalezienie indeksu największej wartości w ograniczonym zakresie
-
-    peak_freq = 1/cepstrum_freqs[limited_cepstrum_freqs][peak_idx] # znalezienie częstotliwości odpowiadającej indeksowi
-
-    real_gender = file.split(".")[0][-1]
-
-    if peak_freq <= 170:
-        detected_gender = "M"
-    else:
-        detected_gender = "K"
-
-    if real_gender == "M":
-        if detected_gender == "M":
-            err_matrix[0][0] += 1
-        else:
-            err_matrix[0][1] += 1
-    else:
-        if detected_gender == "K":
-            err_matrix[1][1] += 1
-        else:
-            err_matrix[1][0] += 1
-            
-
-    if real_gender == detected_gender:
-        good += 1
-    else:
-        bad += 1
-
-
-print(good)
-print(bad)
-# print(detected_gender)
-print(err_matrix)
-
-##########
-
-# import scipy.io.wavfile as wav
-# import matplotlib.pyplot as plt
-# import scipy
-# import numpy as np
-# import os
-
-
-# K = []
-# M = []
+try:
+    file = sys.argv[1]
+except:
+    print("Podaj nazwę pliku w pierwszym argumencie")
+    exit(-1)
 
 # for file in os.listdir("train"):
-#     fs, dataB = wav.read(f'train/{file}')
-#     if len(dataB.shape) > 1:
-#         dataB = dataB[:, 0] 
-    
-#     for i in range(3):
-#         data = dataB[i*len(dataB)//3:(i+1)*len(dataB)//3]
-#         # freqs = np.linspace(0, fs, len(data), endpoint=False)
-#         freqs = np.fft.fftfreq(len(data), 1/fs)
-            
-#         window = scipy.signal.windows.blackman(len(data))
+# fs, data = wav.read(f"train/{file}") # wczytanie pliku
+fs, data = wav.read(file) # wczytanie pliku
 
-#         data = data * window
+if len(data.shape) > 1:
+    data = data[:, 0] # jeśli plik jest stereo, to bierzemy tylko jeden kanał
 
-#         spectrum = (abs(np.fft.fft(data))[0:int(len(data)/2)])/fs
+try:
+    data = medfilt(data, 3) # filtr medianowy
+except:
+    pass
 
-#         num = 5
-#         spectrumCpy = spectrum.copy()
-#         for i in range(2, num):
-#             downsampled = spectrum[::i].copy()
-#             spectrumCpy = spectrumCpy[:len(downsampled)]
-#             spectrumCpy = spectrumCpy * downsampled
-        
-#         print(freqs[np.argmax(spectrumCpy)])
-            
-        # gender = file.split(".")[0][-1]
-        # if gender == "K":
-        #     K.append(freqs[np.argmax(spectrumCpy)])
-        # else:
-        #     M.append(freqs[np.argmax(spectrumCpy)])
+freqs = np.fft.fftfreq(len(data), 1/fs)  # obliczenie częstotliwości
 
-# print(K)
-# print(M)
-# T = 5
-# fs, dataB = wav.read(f'train/015_K.wav')
-# if len(dataB.shape) > 1:
-#     dataB = dataB[:, 0] 
+window = scipy.signal.windows.flattop(len(data)) # okno flattopa
 
-# for i in range(T):
-#     data = dataB[i*len(dataB)//T:(i+1)*len(dataB)//T]
-#     # freqs = np.linspace(0, fs, len(data), endpoint=False)
-#     freqs = np.fft.fftfreq(len(data), 1/fs)
-        
-#     window = scipy.signal.windows.blackman(len(data))
+data = data * window # zastosowanie okna
 
-#     data = data * window
+spectrum = np.fft.fft(data) # obliczenie spektrum
 
-#     # spectrum = (abs(np.fft.fft(data))[0:int(len(data)/2)])/fs
-#     spectrum = abs(np.fft.fft(data))/fs
+log_spectrum = np.log(np.abs(spectrum)) # logarytmowanie
 
-#     num = 5
-#     spectrumCpy = spectrum.copy()
-#     for i in range(2, num):
-#         downsampled = spectrum[::i].copy()
-#         spectrumCpy = spectrumCpy[:len(downsampled)]
-#         spectrumCpy = spectrumCpy * downsampled
-    
-#     print(freqs[np.argmax(spectrumCpy)])
+cepstrum = np.abs(np.fft.fft(log_spectrum)) # obliczenie cepstrum
 
-# fs, data = wav.read(f'train/015_K.wav')
-# if len(data.shape) > 1:
-#     data = data[:, 0]
-    
-# # plt.plot(data)
-# # plt.show()
-    
-# freqs = np.fft.fftfreq(len(data), 1/fs)
-    
-# window = scipy.signal.windows.blackman(len(data))
+cepstrum_freqs = np.fft.fftfreq(len(log_spectrum), (freqs[1]-freqs[0])) # obliczenie częstotliwości cepstrum
 
-# data = data * window
+human_freqs_range = (50, 300)
 
-# # plt.plot(data)
-# # plt.show()
+limited_cepstrum_freqs = (cepstrum_freqs > 1/human_freqs_range[1]) & (cepstrum_freqs <= 1/human_freqs_range[0]) # ograniczenie zakresu częstotliwości
 
-# spectrum = (np.abs(np.fft.fft(data))) / fs
+peak_idx = np.argmax(cepstrum[limited_cepstrum_freqs]) # znalezienie indeksu największej wartości w ograniczonym zakresie
 
-# plt.plot(spectrum)
-# plt.xscale("log")
-# plt.show()
+peak_freq = 1/cepstrum_freqs[limited_cepstrum_freqs][peak_idx] # znalezienie częstotliwości odpowiadającej indeksowi
 
-# num = 5
-# spectrumCpy = spectrum.copy()
-# for i in range(2, num):
-#     downsampled = spectrum[::i].copy()
-#     spectrumCpy = spectrumCpy[:len(downsampled)]
-#     spectrumCpy = spectrumCpy * downsampled
-    
-# human_freqs_range = (50, 300)
+real_gender = file.split(".")[0][-1]
 
-# freqs = freqs[:len(spectrumCpy)]
+if peak_freq <= 170:
+    detected_gender = "M"
+else:
+    detected_gender = "K"
 
-# limited_cepstrum_freqs = (freqs > 1/human_freqs_range[1]) & (freqs <= 1/human_freqs_range[0]) # ograniczenie zakresu częstotliwości
+print(detected_gender)
 
+    # if real_gender == "M":
+    #     if detected_gender == "M":
+    #         err_matrix[0][0] += 1
+    #     else:
+    #         err_matrix[0][1] += 1
+    # else:
+    #     if detected_gender == "K":
+    #         err_matrix[1][1] += 1
+    #     else:
+    #         err_matrix[1][0] += 1
 
-
-# print(spectrumCpy[limited_cepstrum_freqs])
-
-# peak_idx = np.argmax(spectrumCpy[limited_cepstrum_freqs]) # znalezienie indeksu największej wartości w ograniczonym zakresie
-
-# peak_freq = 1/spectrumCpy[limited_cepstrum_freqs][peak_idx] # znalezienie częstotliwości odpowiadającej indeksowi
-
-# print(peak_freq)
-
-# print(freqs)
-# print(np.argmax(spectrumCpy))
-# print(freqs[np.argmax(spectrumCpy)])
-
-
-# freqs = np.linspace(0, fs, len(data), endpoint=False)
-# plt.stem(freqs, spectrum, ".")
-# plt.plot(spectrum)
-# plt.xscale("log")
-# plt.show()
-
-# plt.plot(spectrum/fs)
-# plt.xscale("log")
-# plt.show()
-
-# reduced versions of spectrum
-# reduced_spectrums = []
-
-# print(len(reduced_spectrums))
-
-# min_len = len(reduced_spectrums[-1])
-# hps = reduced_spectrums[-1]
-# for i in range(len(reduced_spectrums)-2, -1, -1):
-#     hps = hps * reduced_spectrums[i][:min_len]
-
-# for i in range(2, num):
-    
-
-# plt.plot(hps)
-# stem(freqs[:len(hps)], hps, '-*')
-# plt.xscale("log")
-# plt.show()
-
-# stem(freqs[:len(hps)], hps, '-*')
-# plt.xscale("log")
-# plt.show()
-
-# print(freqs[np.argmax(hps)])
+# print(err_matrix)
 
